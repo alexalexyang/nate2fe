5c80584828ef96b31643b9ba0885f8f11eed926c
alexalexyang <alexalexyang@gmail.com>
2021-02-20 16:47:24 +0100

    Center Loading component with flex

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,15 +1,23 @@
-import { Container } from "@material-ui/core";
 import { NextPage } from "next";
 import PlayLottie from "../utils/play-lottie";
+import styled from "styled-components";
+
+export const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
 
 let LoadingAnim = require(`../assets/loading.json`);
 
 const Loading: NextPage = () => {
   return (
-    <Container maxWidth="sm">
+    <Center>
       {PlayLottie(LoadingAnim, 300, 300)}
       <h2>{`This is you. This is how you wait.`}</h2>
-    </Container>
+    </Center>
   );
 };
 
