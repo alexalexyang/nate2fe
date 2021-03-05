1a2a8848b2c53e9f59724f577fb2010873507bae
alexalexyang <alexalexyang@gmail.com>
2021-03-05 18:35:27 +0100

    Add seo

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { NextPage } from "next";
+import { NextSeo } from "next-seo";
 import React from "react";
 import getConfig from "next/config";
-
 const { publicRuntimeConfig } = getConfig();
 
 interface Props {
@@ -11,14 +11,46 @@ interface Props {
 
 const PageHead: NextPage<Props> = ({ page }: Props) => {
   const pageTitle = page ? `${page} | ` : null;
+  const appDescription = "i am not a cat";
+  const appName = "supermeowkat";
+  const appUrl = "https://supermeowkat.com";
 
   return (
-    <Head>
-      <title>
-        {pageTitle}
-        {publicRuntimeConfig.APP_NAME}
-      </title>
-    </Head>
+    <>
+      <meta name="theme-color" content="#4285f4" />
+      <NextSeo
+        title={`${pageTitle}${appName}`}
+        description={appDescription}
+        canonical={appUrl}
+        openGraph={{
+          url: appUrl,
+          title: appName,
+          description: appDescription,
+          // images: [
+          //   {
+          //     url: "https://www.example.ie/og-image-01.jpg",
+          //     width: 800,
+          //     height: 600,
+          //     alt: "Og Image Alt",
+          //   },
+          //   {
+          //     url: "https://www.example.ie/og-image-02.jpg",
+          //     width: 900,
+          //     height: 800,
+          //     alt: "Og Image Alt Second",
+          //   },
+          //   { url: "https://www.example.ie/og-image-03.jpg" },
+          //   { url: "https://www.example.ie/og-image-04.jpg" },
+          // ],
+          site_name: appName,
+        }}
+        twitter={{
+          handle: "@supermeowkat",
+          site: appUrl,
+          cardType: "summary_large_image",
+        }}
+      />
+    </>
   );
 };
 
